82781285a425f9aa24f2b13c5e4d1688f6a4f42f
dishant <dishantgandhi99@yahoo.com>
2019-07-04 02:53:18 +0530

    First commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    document.querySelector('#addtask').disabled = true;
+    document.querySelector('#removetask').disabled = true;
+    document.querySelector('#text').onkeyup = () =>{
+       if(document.querySelector('#text').value.length > 0){
+        document.querySelector('#addtask').disabled = false;
+       }
+       else{
+        document.querySelector('#addtask').disabled = true;
+       }
+    }
+    document.querySelector('#task').onsubmit = () =>{
+       let li = document.createElement('li');
+        li.innerHTML =  document.querySelector('#text').value;
+            document.querySelector('#newtasks').append(li);
+            document.querySelector('#removetask').disabled = false;
+            return false;
+    }
+    document.querySelector('#removetask').onclick = () =>{
+        var a =  document.querySelectorAll('li');
+        if(a.length == 1){
+        document.querySelector('li').remove();
+        document.querySelector('#removetask').disabled = true;
+        }
+        else
+        {
+            document.querySelector('li').remove();
+        }
+    }
+});
